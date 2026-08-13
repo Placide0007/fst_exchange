3336ee72b197e135d9b801c7bd44d1227ed5d630
Placide <lazareplacide02@gmail.com>
2026-08-13 13:40:08 +0200

    feat:mise en place des pages d'authentification

--- core/core_urls.py
+++ core/core_urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home,name="home"),
+    path("profile/", views.profile,name="profile"),
+    path("mathematics/", views.math,name="math"),
+    path("pc/", views.pc,name="pc"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 def home(request):
     return render(request,'core/home.html')
 
+def profile(request):
+    return render(request,'core/profile.html')
+
+def math(request):
+    return render(request,'core/academic_program/math_info/math.html')
+
+def pc(request):
+    return render(request,'core/academic_program/pc/pc.html')
+

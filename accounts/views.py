6f601b8092e2f29a4890fb4b4f9709095679ab05
Placide <lazareplacide02@gmail.com>
2026-08-14 22:43:34 +0200

    feat:ajout des logiques metier de auth

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login , logout
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
 
-# Create your views here.
-def login(request):
-    return render(request,'accounts/login.html')
+
+def login_view(request):
+    
+    form = AuthenticationForm(request, data=request.POST or None)
+
+    if form.is_valid():
+        login(request, form.get_user())
+        return redirect("home")
+
+    return render(request, "accounts/login.html", {"form": form})
 
 def register(request):
-    return render(request,'accounts/register.html')
+
+    form = RegisterForm(request.POST or None)
+
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect("home") 
+
+    return render(request, "accounts/register.html", {"form": form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
